d6287a6f4cab305068296a911537129b8dd21080
Sushanta Bhowmick <bhosushanta922@gmail.com>
2024-09-09 02:17:30 +0530

    add authentication and its middleware

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 // import db from "@/lib/supabase/db";
 import { ThemeProvider } from "@/lib/providers/next-themes-providers";
